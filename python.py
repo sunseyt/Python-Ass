7693c7dbc9c5b5f125dc87475ac8b394021194be
sunseyt <61967000+sunseyt@users.noreply.github.com>
2020-03-26 16:44:27 +0100

    Add files via upload

---
+++ python.py
@@ -0,0 +1,10 @@
+print ('Hi')
+print ('I am Richard')
+print ('this is the answer to python assignment')
+radius = int(input('what is the radius of the circle'))
+print ('The radius of the circle is', radius, 'cm')
+print ('What then is the area of a cirle, given this radius')
+import math
+pi = math.pi
+area = pi * radius * radius
+print ('The area of the circle given this radius is', area, 'cm^2')
